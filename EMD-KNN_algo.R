1c813209e039cfb0d56c7c9a68363a1075738bb4
Niknymusing <nilskakoseos@gmail.com>
2020-02-19 22:51:58 +0100

    Update EMD-KNN_algo.R

--- EMD-KNN_algo.R
+++ EMD-KNN_algo.R
@@ -21,7 +21,7 @@ j=960
 # length of forecast, look-ahead timewindow is 960 hours/pricepoints.
 
 l=24 
-# length of look-back timewindow, for computing the k nearest neighboors of length-j segments
+# length of look-back timewindow, for computing the k nearest neighboors of consecutive length-j segments.
 
 k=1060 
 # number of nearest neigboors (number of weighted training examples to add together to construct the forecast)
@@ -31,9 +31,7 @@ forecast <- vector(mode = "numeric", length=j)
 for(i in 1:10) {
 
 sigimf <- sigemd$imf[,i]
-
 query_timewindow <- sigimf[(length(sigimf) - j - l):(length(sigimf) - j - 1)] 
-
 Training_data <- matrix(ncol = l, nrow = 0)
 
 for(i in 1:(length(sigimf) - j - l)) {        
@@ -45,13 +43,14 @@ Training_data <- rbind(Training_data, t(sigimf[(i):(i + l - 1)]))
 
 query_dist <- matrix(ncol = 0, nrow = 1)
 
+
 for(i in 1:(length(sigimf) - j - 2*l)) {
 
 D <- dist(rbind(Training_data[i,], t(query_timewindow)))        
-
 query_dist <- cbind(query_dist,  D)         
-
+ 
 }
+ 
 # Computes the euclidean distance of [all l-segments in the training dataset] and [the last segment of 
 # the timseries (query_timewindow above)], and puts the resulting distance-coefficients in the query_dist object. 
 
@@ -64,7 +63,6 @@ f <- vector(mode = "numeric", length=j)
 for(i in 1:k) {
 
 q <- 1/(dist(rbind(Training_data[indices_of_k_nearest_neighboors[i],], t(query_timewindow))) + 0.01)
-
 f <- f + q*sigimf[(indices_of_k_nearest_neighboors[i] + l):(indices_of_k_nearest_neighboors[i] + l + j - 1)]
 
 }
@@ -72,13 +70,16 @@ f <- f + q*sigimf[(indices_of_k_nearest_neighboors[i] + l):(indices_of_k_nearest
  
 c = 0
 
-for(i in 1:k) 
+for(i in 1:k) {
 
 c <-  c + 1/(dist(rbind(Training_data[indices_of_k_nearest_neighboors[i]], t(query_timewindow))) + 0.01) 
 
+}
+# Computes normalisation constant for the convex combination of weighted k-nearest neighboors.
+
+
 forecast <- forecast + (1/c)*f
 
-# Computes normalisation constant for the convex combination of weighted k-nearest neighboors.
 
 } 
 
@@ -95,6 +96,7 @@ Training_data <- matrix(ncol = l, nrow = 0)
 for(i in 1:(length(sigres) - j - l)) {        
 
 Training_data <- rbind(Training_data, t(sigres[(i):(i + l - 1)]))
+
 }
 
 L <- matrix(ncol = l, nrow = 0)
@@ -103,8 +105,8 @@ query_dist <- matrix(ncol = 0, nrow = 1)
 
 for(i in 1:(length(sigimf) - j - 2*l)) {
 
-D <- dist(rbind(Training_data[i,], t(query_timewindow)))        
 
+ D <- dist(rbind(Training_data[i,], t(query_timewindow)))        
 query_dist <- cbind(query_dist,  D)         
 
 }
@@ -116,15 +118,16 @@ f <- vector(mode = "numeric", length=j)
 for(i in 1:k) {
 
 q <- 1/(dist(rbind(Training_data[indices_of_k_nearest_neighboors[i],], t(query_timewindow))) + 0.01)
-
 f <- f + q*sigres[(indices_of_k_nearest_neighboors[i]+l):(indices_of_k_nearest_neighboors[i] + l + j - 1)]
 
 }
  
 c = 0
 
-for(i in 1:k) 
+for(i in 1:k) {
 
 c <-  c + 1/(dist(rbind(Training_data[indices_of_k_nearest_neighboors[i]], t(query_timewindow))) + 0.01) 
 
+} 
+ 
 forecast <- forecast + (1/c)*f
